350bf3084c447a6f5d1b894a9bef429d420a4d13
Tran Toan <huutrantoan@gmail.com>
2022-10-26 19:29:09 +0700

    feat(api): add base controller

--- server/src/user/userController.js
+++ server/src/user/userController.js
@@ -1,42 +1,12 @@
 import User from "./userModel";
+const base = require("../utils/baseController");
 
 export default {
-  getUser: async (req, res, next) => {
-    try {
-      const user = await User.findById(req.params.id);
-      res.status(200).json(user);
-    } catch (err) {
-      next(err);
-    }
-  },
+  getUser: base.getOne(User),
 
-  getUsers: async (req, res, next) => {
-    try {
-      const users = await User.find();
-      res.status(200).json(users);
-    } catch (err) {
-      next(err);
-    }
-  },
-  updateUser: async (req, res, next) => {
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        { $set: req.body },
-        { new: true }
-      );
-      res.status(200).json(updatedUser);
-    } catch (err) {
-      next(err);
-    }
-  },
+  getAllUsers: base.getAll(User),
 
-  deleteUser: async (req, res, next) => {
-    try {
-      await User.findByIdAndDelete(req.params.id);
-      res.status(200).json("User has been deleted.");
-    } catch (err) {
-      next(err);
-    }
-  },
+  updateUser: base.updateOne(User),
+
+  deleteUser: base.deleteOne(User),
 };
--- server/src/user/userRouter.js
+++ server/src/user/userRouter.js
@@ -2,12 +2,12 @@ import userController from "./userController";
 
 const router = require("express").Router();
 
-router.get("/", userController.getUsers);
+router.route("/").get(userController.getAllUsers);
 
-router.get("/:id", userController.getUser);
-
-router.put("/:id/update", userController.updateUser);
-
-router.delete("/:id/delete", userController.deleteUser);
+router
+  .route("/:id")
+  .get(userController.getUser)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser);
 
 export default router;
