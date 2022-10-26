d6edfb78380089eef71e0d5345a2dd90d0751c1c
Tran Toan <huutrantoan@gmail.com>
2022-10-26 23:51:46 +0700

    refactor(api)

--- server/src/api/middlewares.js
+++ server/src/api/middlewares.js
@@ -1,29 +1,60 @@
 import User from "../user/userModel";
 import token from "../utils/token";
 
-export default {
-  auth: (req, res, next) => {
-    if (!req.header("Authorization"))
-      return res.status(401).send({
-        message: "Please make sure your request has an Authorization header.",
-      });
-
-    // Validate jwt
-    let try_token = req.header("Authorization").split(" ")[1];
-    token.verifyToken(try_token, (err, payload) => {
-      if (err) return res.status(401).send(err);
-      User.findById(payload.sub).exec((err, user) => {
-        if (err || !user) {
-          return res.status(404).send(
-            err || {
-              error: "Middleware: User not found!!!",
-            }
-          );
-        }
-        delete user.password;
-        req.user = user;
-        next();
-      });
+export const auth = (req, res, next) => {
+  if (!req.header("Authorization"))
+    return res.status(401).send({
+      message: "Please make sure your request has an Authorization header.",
     });
-  },
+
+  // Validate jwt
+  let try_token = req.header("Authorization").split(" ")[1];
+  token.verifyToken(try_token, (err, payload) => {
+    if (err) return res.status(401).send(err);
+    User.findById(payload.sub).exec((err, user) => {
+      if (err || !user) {
+        return res.status(404).send(
+          err || {
+            error: "User not found",
+          }
+        );
+      }
+      delete user.password;
+      req.user = user;
+      next();
+    });
+  });
+};
+
+export const admin = (req, res, next) => {
+  if (!req.header("Authorization"))
+    return res.status(401).send({
+      message: "Please make sure your request has an Authorization header.",
+    });
+
+  // Validate jwt
+  let try_token = req.header("Authorization").split(" ")[1];
+  token.verifyToken(try_token, (err, payload) => {
+    if (err) return res.status(401).send(err);
+    User.findById(payload.sub).exec((err, user) => {
+      if (err || !user) {
+        return res.status(404).send(
+          err || {
+            error: "User not found",
+          }
+        );
+      } else if (!user.isAdmin) {
+        return res.status(404).send(
+          err || {
+            error: "You are not allowed",
+          }
+        );
+      }
+
+      delete user.password;
+      console.log(user);
+      req.user = user;
+      next();
+    });
+  });
 };
--- server/src/user/userRouter.js
+++ server/src/user/userRouter.js
@@ -1,8 +1,9 @@
+import { admin } from "../api/Middlewares";
 import userController from "./userController";
 
 const router = require("express").Router();
 
-router.route("/").get(userController.getAllUsers);
+router.get("/", admin, userController.getAllUsers);
 
 router
   .route("/:id")
