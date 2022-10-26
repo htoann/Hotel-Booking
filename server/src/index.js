1ce7242a3349157c3d511f27ec1824afa1c6ddde
Tran Toan <huutrantoan@gmail.com>
2022-10-26 13:04:21 +0700

    Init

--- server/src/index.js
+++ server/src/index.js
@@ -1,47 +1,57 @@
-import http from 'http';
-import express from 'express';
-import morgan from 'morgan';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import logger from './util/logger';
-import config from './config';
-import Middlewares from './api/middlewares'
-import Authentication from './api/authentication'
-import UserRouter from './user/router'
+import http from "http";
+import express from "express";
+import morgan from "morgan";
+import mongoose from "mongoose";
+import cors from "cors";
+import logger from "./util/logger";
+import config from "./config";
+import Middlewares from "./api/middlewares";
+import Authentication from "./api/authentication";
+import UserRouter from "./user/router";
 
-if(!process.env.JWT_SECRET) {
-    const err = new Error('No JWT_SECRET in env variable, check instructions: https://github.com/amazingandyyy/mern#prepare-your-secret');
-    logger.warn(err.message);
+if (!process.env.JWT_SECRET) {
+  const err = new Error("No JWT_SECRET in env variable");
+  logger.warn(err.message);
 }
 
 const app = express();
 
-mongoose.connect(config.mongoose.uri, { useNewUrlParser: true, useUnifiedTopology: true })
-.catch(err=>console.error(err));
+mongoose
+  .connect(config.mongoose.uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.error(err));
 
 mongoose.Promise = global.Promise;
 
 // App Setup
-app.use(cors({
-    origin: ['https://www.amazingandyyy.com', 'http://localhost:3000']
-}));
-app.use(morgan('dev'));
-app.use(express.json())
+app.use(
+  cors({
+    origin: ["https://www.amazingandyyy.com", "http://localhost:3000"],
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.get('/ping', (req, res) => res.send('pong'))
-app.get('/', (req, res) => res.json({'source': 'https://github.com/amazingandyyy/mern'}))
-app.post('/signup', Authentication.signup)
-app.post('/signin', Authentication.signin)
-app.get('/auth-ping', Middlewares.loginRequired, (req, res) => res.send('connected'))
-app.use('/user', Middlewares.loginRequired, UserRouter)
+app.get("/ping", (req, res) => res.send("pong"));
+app.get("/", (req, res) =>
+  res.json({ source: "https://github.com/amazingandyyy/mern" })
+);
+app.post("/signup", Authentication.signup);
+app.post("/signin", Authentication.signin);
+app.get("/auth-ping", Middlewares.loginRequired, (req, res) =>
+  res.send("connected")
+);
+app.use("/user", Middlewares.loginRequired, UserRouter);
 
 app.use((err, req, res, next) => {
-    logger.error(err.message);
-    res.status(422).json(err.message);
+  logger.error(err.message);
+  res.status(422).json(err.message);
 });
 
 // Server Setup
-const port = process.env.PORT || 8000
-http.createServer(app).listen(port, ()=>{
-    logger.info(`Server listening on: ${port}`)
+const port = process.env.PORT || 8000;
+http.createServer(app).listen(port, () => {
+  logger.info(`Server listening on: ${port}`);
 });
