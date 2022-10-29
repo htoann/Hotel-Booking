06ff2ee0b2529776a857464d3ae0820a5eea5c82
htoann <huutrantoan@gmail.com>
2022-10-29 16:27:18 +0700

    feat(api): get hotel rooms

--- server/src/hotel/hotelRouter.js
+++ server/src/hotel/hotelRouter.js
@@ -3,15 +3,16 @@ import hotelController from "./hotelController";
 
 const router = require("express").Router();
 
-
 router.post("/", admin, hotelController.createHotel);
 
 router
-.route("/:id", admin)
-.put(hotelController.updateHotel)
-.delete(hotelController.deleteHotel);
+  .route("/:id", admin)
+  .put(hotelController.updateHotel)
+  .delete(hotelController.deleteHotel);
 
 router.get("/search/:id", hotelController.getHotel);
 router.get("/", hotelController.getAllHotels);
+
+router.get("/room/:id", hotelController.getHotelRooms);
 
 export default router;
