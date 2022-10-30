4c6f275462102f74348cc520b1028be2bbe3146d
Tran Toan <huutrantoan@gmail.com>
2022-10-30 21:08:04 +0700

    fix(api): response message

--- server/src/utils/createMessage.js
+++ server/src/utils/createMessage.js
@@ -2,11 +2,9 @@ export const createError = (res, status, message) => {
   const err = new Error();
   err.status = status || 500;
   err.message = message || "Something went wrong";
-  return res.status(status).send({
-    data: {
-      status,
-      message,
-    },
+  return res.send({
+    status,
+    message,
   });
 };
 
@@ -14,10 +12,8 @@ export const createMessage = (res, status, message) => {
   const err = new Error();
   err.status = status || 200;
   err.message = message || "Successfully";
-  return res.status(status).send({
-    data: {
-      status,
-      message,
-    },
+  return res.send({
+    status,
+    message,
   });
 };
