da2b76393df0c8b2af54d4fda15d770634ca2a43
htoann <huutrantoan@gmail.com>
2022-11-16 13:20:21 +0700

    fix(api): app cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,22 @@
-import app from "./app";
 import route from "./routes";
 import logger from "./utils/logger";
 import initDatabase from "./initDatabase";
+
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+// App Setup
+app.use(
+  cors({
+    origin: ["https://bookinghotels.vercel.app", "http://localhost:3000"],
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 initDatabase();
 
