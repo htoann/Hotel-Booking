faffc9327381230792ac1cb694645de52e91a4cb
Tran Toan <huutrantoan@gmail.com>
2022-11-18 23:47:51 +0700

    fix(client): wishlist hotels

--- client/pages/wishlist/index.tsx
+++ client/pages/wishlist/index.tsx
@@ -3,32 +3,38 @@ import React from "react";
 import HotelPreview from "../../components/hotel/HotelPreview";
 import { Loader } from "../../components/layout";
 import { useGetUserQuery } from "../../services/userApi";
+import { useAppSelector } from "../../store/hooks";
 
 const Index = () => {
-	const { data: user, isLoading } = useGetUserQuery();
+  const { data: user, isLoading } = useGetUserQuery();
+  const { wishList } = useAppSelector((state) => state.persistedReducer.app);
 
-	if (isLoading) {
-		return <Loader />;
-	}
-
-	return (
-		<div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-2 mt-8 lg:grid-cols-3 justify-center mx-auto max-w-screen-xl overflow-hidden ">
-			<Head>
-				<title>Wishlist of {user?.username}</title>
-			</Head>
-			{user?.wishlist?.map((hotel: any) => (
-				<div key={hotel._id}>
-					<HotelPreview
-						id={hotel._id}
-						image={hotel.photos[0]}
-						name={hotel.name}
-						title={hotel.title}
-						large
-					/>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <>
+      {isLoading ? (
+        <div className="w-screen mt-20 flex items-center justify-center">
+          <Loader />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-2 mt-8 lg:grid-cols-3 justify-center mx-auto max-w-screen-xl overflow-hidden ">
+          <Head>
+            <title>Wishlist of {user?.username}</title>
+          </Head>
+          {wishList.length !== 0 ? (
+            wishList.map((id: string) => (
+              <div key={id}>
+                <HotelPreview id={id} large />
+              </div>
+            ))
+          ) : (
+            <div className="w-screen mt-20 flex items-center justify-center">
+              <h1 className="font-bold text-3xl	">No hotels saved</h1>
+            </div>
+          )}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Index;
