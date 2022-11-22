2128698584f683288146c1a39baf96b3d55e1152
Tran Toan <huutrantoan@gmail.com>
2022-11-22 19:02:14 +0700

    feat(client): booking room

--- client/services/roomApi.ts
+++ client/services/roomApi.ts
@@ -1,18 +1,32 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {IRoom} from '../models'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { IRoom } from "../models";
+import {RootState} from '../store/store'
 
-import {apiUrl} from '../utils/config'
+import { apiUrl } from "../utils/config";
 
 export const roomApi = createApi({
-    reducerPath: 'roomApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: apiUrl
-    }),
-    endpoints: (builder) => ({
-        getHotelRooms: builder.query<IRoom[], string>({
-            query: (id) => `/hotels/room/${id}`
-        })
-    })
-})
+  reducerPath: "roomApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: apiUrl,
+    prepareHeaders: (headers, {getState, endpoint}) => {
+            const user = (getState() as RootState).persistedReducer.auth
 
-export const {useGetHotelRoomsQuery} = roomApi
+            if (user && user.token && endpoint !== 'refresh') {
+                headers.set('Authorization', `Bearer ${user.token}`)
+            }
+            return headers
+        }
+  }),
+  endpoints: (builder) => ({
+    getHotelRooms: builder.query<IRoom[], string>({
+      query: (id) => `/hotels/room/${id}`,
+    }),
+    bookingRoom: builder.mutation({
+      query: (body: { roomId: string ; checkIn: Date; checkOut: Date, price: Number }) => {
+        return { url: "/booking", method: "post", body };
+      },
+    }),
+  }),
+});
+
+export const { useGetHotelRoomsQuery, useBookingRoomMutation } = roomApi;
