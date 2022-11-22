90643a31b04d02d8985def3568d299ec53d1e597
Tran Toan <huutrantoan@gmail.com>
2022-11-22 10:17:48 +0700

    fix(api): token not null

--- server/src/api/middlewares.js
+++ server/src/api/middlewares.js
@@ -3,15 +3,22 @@ import { createError } from "../utils/createMessage";
 import token from "../utils/token";
 
 export const auth = (req, res, next) => {
-  if (!req.header("Authorization"))
+  let try_token;
+  if (
+    req.header("Authorization") &&
+    req.header("Authorization").startsWith("Bearer")
+  ) {
+    try_token = req.header("Authorization").split(" ")[1];
+  }
+
+  if (!try_token) {
     return createError(
       res,
       401,
       "Please make sure your request has an Authorization header."
     );
+  }
 
-  // Validate jwt
-  let try_token = req.header("Authorization").split(" ")[1];
   token.verifyToken(try_token, (err, payload) => {
     if (err) return createError(res, 401, "User has been created.");
     User.findById(payload.sub).exec((err, user) => {
@@ -26,25 +33,31 @@ export const auth = (req, res, next) => {
 };
 
 export const admin = (req, res, next) => {
-  if (!req.header("Authorization"))
+  let try_token;
+  if (
+    req.header("Authorization") &&
+    req.header("Authorization").startsWith("Bearer")
+  ) {
+    try_token = req.header("Authorization").split(" ")[1];
+  }
+
+  if (!try_token) {
     return createError(
       res,
       401,
       "Please make sure your request has an Authorization header."
     );
+  }
 
-  // Validate jwt
-  let try_token = req.header("Authorization").split(" ")[1];
   token.verifyToken(try_token, (err, payload) => {
-    if (err) return next(createError(res, 401, err));
+    if (err) return createError(res, 401, "User has been created.");
     User.findById(payload.sub).exec((err, user) => {
       if (err || !user) {
-        return createError(res, 404, err || "You are not allowed");
+        return createError(res, 404, err || "User not found");
       }
       if (user.isAdmin == false) {
         return createError(res, 404, err || "You are not allowed");
       }
-
       delete user.password;
       req.user = user;
       next();
