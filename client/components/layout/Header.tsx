4d80f3b709e4c83713e89bb27d64225dc2e335ac
htoann <huutrantoan@gmail.com>
2022-11-30 12:37:30 +0700

    fix(client): set booking null logout

--- client/components/layout/Header.tsx
+++ client/components/layout/Header.tsx
@@ -19,7 +19,7 @@ import {useAppDispatch, useAppSelector} from '../../store/hooks'
 import {logout} from '../../features/authSlice'
 import {toast} from 'react-toastify'
 import {useRouter} from 'next/router'
-import {setHotelWishList} from '../../features/appSlice'
+import {setBookings, setHotelWishList} from '../../features/appSlice'
 
 const Header = () => {
     const router = useRouter()
@@ -30,6 +30,7 @@ const Header = () => {
     const handleLogout = async () => {
         dispatch(logout())
         dispatch(setHotelWishList([]))
+        dispatch(setBookings([]))
         toast.success('User logged out...')
         await router.push('/auth')
     }
